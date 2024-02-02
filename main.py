146ae10fd2e220c80e850a7451877f9bc319e76b
gianflores <gian_flores@hotmail.com>
2024-02-02 07:58:31 -0500

    update

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello")
+from time import sleep
+import math
+from clicknium import clicknium as cc
+
+
+def circle():
+    a, b = cc.mouse.position()
+    w = 20
+    m = (2 * math.pi) / w
+    r = 200
+
+    while 1:
+        for i in range(0, w + 1):
+            x = int(a + r * math.sin(m * i))
+            y = int(b + r * math.cos(m * i))
+            cc.mouse.move(x, y)
+            sleep(0.2)
+
+
+if __name__ == "__main__":
+    circle()
+
+# import pyautogui
+# import time
+
+# while True:
+#     pyautogui.click(x=100, y=100)
+#     pyautogui.press("enter")
+#     time.sleep(5)
+#     pyautogui.click(x=200, y=200)
+#     pyautogui.press("enter")
+#     time.sleep(5)
